a57b224e4589dbbe8b1b23649b9149387b6d3197
nicholasroberson <roberson@grinnell.edu>
2018-02-25 12:55:13 -0600

    added tests and more formatting to enumeration.scm and driver.scm

--- driver.scm
+++ driver.scm
@@ -1,13 +1,53 @@
+;;
+;; File
+;;   driver.scm
+;;
+;; Author
+;;   Jerod Weinman (Documentation)
+;;
+;; Summary
+;;   Provides a collection of tests for the (tt-entails? knowledge-base query),
+;;   (tt-check-all knowledge-base query symbols model), and (pl-true? sentence
+;;   model) functions.
+;;
+;; Provides
+;;   (pl-true?-tests)
+;;   (tt-entails?-tests)
+;;   (tt-check-all-tests)
+;;
+;; Practica
+;;   A sentence is either a boolean value, a symbol (representing a
+;;   variable), or a list. When the sentence is a list, the first item
+;;   must be one of the symbols 'not, 'and, 'or, '=>, or '<=>. If the
+;;   symbol is 'not, the list must have only a second element, another
+;;   sentence, otherwise the list must have two subsequent elements,
+;;   sentences, that represent the arguments to the specified logical
+;;   connectives.
+;;
+;;   More formally, a sentence is defined as follows:
+;; 
+;;     sentence := boolean | symbol | (not sentence) | 
+;;                 (and sentence sentence) |
+;;                 (or sentence sentence) |
+;;                 (=> sentence sentence) |
+;;                 (<=> sentence sentence)
+;;
+;;   A model is an association list whose keys (car of elements) are
+;;   symbols and whose values (cdr of elements) are booleans.(require rackunit)
+
+; load necessary libraries
 (require rackunit)
 (require rackunit/text-ui)
 (load "logic.scm")
 (load "enumeration.scm")
 
+; instantiate data that tests rely on 
 (define model (list (cons 'A #t)
                     (cons 'B #f)
                     (cons 'C #t)
                     (cons 'D #f)))
 
+; tests for (pl-true? sentence model)
 (define pl-true?-tests
   (test-suite
    "Tests for pl-true?"
@@ -60,5 +100,29 @@
     "bidirectional implication #f <=> #f"
     (check-equal? (pl-true? (list '<=> 'B 'D) model) #t))))
 
+; tests for (tt-entails? knowledge-base query)
+(define tt-entails?-tests
+  (test-suite
+   "Tests for tt-entails?"
+   (test-case
+    "none yet"
+    (equal? 0 0))))
 
+; tests for (tt-check-all knowledge-base query symbols model)
+(define tt-check-all-tests
+  (test-suite
+   "Tests for tt-entails?"
+   (test-case
+    "none yet"
+    (equal? 0 0))))
+
+
+; run all tests
+(display "Testing (pl-true? sentence model)...")(newline)
 (run-tests pl-true?-tests)
+
+(display "Testing (tt-entails? knowledge-base query)...")(newline)
+(run-tests tt-entails?-tests)
+
+(display "Testing (tt-check-all knowledge-base query symbols model)...")(newline)
+(run-tests tt-check-all-tests)
